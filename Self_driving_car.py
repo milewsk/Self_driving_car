98486cc288acdc6d5710cd6975c09da20cd043c6
Konrad Milewski <konradmilewski2000@interia.eu>
2021-05-01 11:33:44 +0200

    Dodaj pliki projektów.

---
+++ Self_driving_car.py
@@ -0,0 +1,12 @@
+# Code to perform self driving car based on genetic algorithm
+
+#import must have lib's
+
+import sys
+import math
+import os
+import random
+
+# based on pygame and neat library
+import pygame
+import neat
