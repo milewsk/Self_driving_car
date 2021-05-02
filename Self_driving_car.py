ae91bfdc49c9d2a75802be9cf7cd529f604dd2ff
Konrad Milewski <konradmilewski2000@interia.eu>
2021-05-02 20:51:58 +0200

    Nowe funkcje

--- Self_driving_car.py
+++ Self_driving_car.py
@@ -42,7 +42,7 @@ class car:
         self.model = pygame.image.load("car.png").convert()
         self.model = pygame.transform.scale(self.model, (CAR_SIZE_X,CAR_SIZE_Y))
 
-        self.posision = [0,0] 
+        self.position = [0,0] 
         self.angle = 0 
         self.speed = 0
     
@@ -54,12 +54,14 @@ class car:
 
         # Musimy mieć środek pojazdu w aktualnym położeniu na mapie 
         # Dodamy jego położenie na mapie - użyjemy połowę długości i szerokości aby wyznaczyć środek obieku
-        self.car_center = [posision[0]+(CAR_SIZE_X/2),posision[1]+(CAR_SIZE_Y/2)]
+        self.car_center = [position[0]+(CAR_SIZE_X/2),position[1]+(CAR_SIZE_Y/2)]
 
         #parametry które posłużą nam do wyznaczania kolejnych generacji
         self.alive = True
         self.distance = 0
         self.time = 0
+
+        
 
         # Jeśli któryś z krańców auta dotknie granicy samochód umiera
         def check_collision(self, game_map):
@@ -68,3 +70,40 @@ class car:
                 if game_map.get_at((int(point[0]),int(point[1]))) == end_color:
                     self.alive = False
                     break;
+        # sprawdzanie życia obiektu
+        def is_ailve(self):
+            return self.alive
+
+
+        def get_reward(self):
+            return self.distance / (CAR_SIZE_X/2)
+
+        #obracanie się w okół własnej osi
+        def rotate_center(self,image,angle):
+            rect = image.get_rect()
+            rot_img = pygame.transform.rotate(image,angle)
+            rot_rect = rect.copy()
+            rot_rect.center = rot_img.get_rect().center
+            rot_img = rot_img.subsurface(rotrot_rect).copy()
+
+        def draw(self,screen):
+            screen.blit(self.rotated_sprite, self.position)
+            self.draw_radar(screen)
+        
+        #tworzenie linii i kólka w okół samochodu
+        def draw_radar(self,screen):
+            for radar in self.radars:
+                position = radar[0]
+                pygame.draw.line(screen, (0,255,0), self.center, position, 1)
+                pygame.draw.circle(screen,(0,255,0), position, 5)
+
+        def check_radar(self,degree,game_map):
+            lenght = 0
+            x = int(self.center[0]+math.cos(math.radians(360-(self.angle+degree)))*lenght)
+            y = int(self.center[1]+math.sin(math.radians(360-(self.angle+degree)))*lenght)
+
+            while not game_map.get_at((x,y)) == end_color and lenght < 300:
+                lenght = lenght + 1
+                x = int(self.center[0]+math.cos(math.radians(360-(self.angle+degree)))*lenght)
+                y = int(self.center[1]+math.sin(math.radians(360-(self.angle+degree)))*lenght)
+
